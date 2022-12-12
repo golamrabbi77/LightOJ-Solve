92f545a1a0492a6040a11ebc9b73f8bd53c1cc18
Golam Rabbi Hasan <114388633+golamrabbi77@users.noreply.github.com>
2022-12-13 00:56:18 +0600

    Create Even Odd 1.cpp

---
+++ Even Odd 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,n,k,j;
+    cin>>n;
+    for(i=1;i<=n;i++){
+      cin>>k;  
+        if(k%2==0)
+          cout<<"even"<<endl;
+          else
+          cout<<"odd"<<endl;
+    }
+  
+    
+}
